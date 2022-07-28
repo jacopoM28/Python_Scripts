885435632e080758880bc83c39436ad726695e61
Jacopo Martelossi <62595944+jacopoM28@users.noreply.github.com>
2022-07-28 12:49:06 +0200

    Update TE_AnnoSum.py

--- TE_scripts/TE_AnnoSum.py
+++ TE_scripts/TE_AnnoSum.py
@@ -2,17 +2,26 @@
 
 #################################################################################
 #################################################################################
-##                       TE Annotatio summary script                           ##
+##                                                                             ##   
+##                         TE Annotation summary script                        ##
+##                                                                             ##
 #################################################################################
 #################################################################################
 
-#Script to summarize the results of a TE annotation and assess the "quality" of a consensus library
+#Script to summarize the results of a TE annotation and "quality" of a consensus library
 #Six metrics are currently implemented :
 #           1. Mean length of consensus sequences
-#           2. Number of annotated insertions)
+#           2. Number of annotated insertions
 #           4. Number of "Known" insertions
 #           5. Mean length of insertions
 #           6. Number of significant Blastx results of the 10% longest insertions against a reference TE - derived protein db
+#           7. Number of significant Blastx results on consensuxs sequences
+
+#Dependecies necessary : Blast, Bedtools Trimal
+#Python libraries : Biopython, Pandas
+
+#Author : Jacopo Martelossi
+#E-mail : jacopo.martelossi2@unibo.it
 
 import os
 import sys
@@ -23,6 +32,7 @@ import pandas as pd
 from Bio import SeqIO
 from Bio import AlignIO
 from Bio.Align import AlignInfo
+from Bio.Align.Applications import MafftCommandline
 from Bio.Blast.Applications import NcbiblastxCommandline
 from Bio.Blast.Applications import NcbimakeblastdbCommandline
 
@@ -46,26 +56,39 @@ TE_ProtLib = args.TE_prot
 N_threads = int(args.num_threads)
 LibName = args.Lib_Name
 
+print("\n")
+print("#################################################")
+print("#EVALUATING THE LIBRARY AND THE ANNOTATION...   #")
+print("#################################################")
+print("\n")
+
 mydict = {'Name' : [], "Cons.Len" : [],
-       'N.ins' : [], "Known.Ins" : [],
-       'N.Short.ins' : [], "Mean.Ins.Len" : [],
-       'N.Blastx' : []}
+       'N.ins' : [], "Known.Ins" : [], "Mean.Ins.Len" : [],
+       'N.ins_Blastx' : [], "N.cons_Blastx" : []} 
 
 ################################################################################
 #------------------------Mean library consensus length-------------------------#
 ################################################################################
 
+print("\n")
+print("Calculating mean consensus length...")
+
+#Calculating mean consensus length
 i = 0
 Length = 0
 for seq_record in SeqIO.parse(LIBRARY, "fasta") :
     Length = Length + len(seq_record)
     i = i + 1
-Mean_Len = Length/i                                                             #Calculating mean consensus length
+Mean_Len = Length/i                                                             
+
+print("...Done")
 
 ################################################################################
 #-----------------------GFF working (point 2,3,4,5)----------------------------#
 ################################################################################
 
+print("\n")
+print("Creating summary statistics of the annotation...")
 Known_ins = 0
 
 Scaffold = []
@@ -76,65 +99,112 @@ InsLen = []
 
 with open(GFF) as f:
     first_line = f.readline()
-    for line in f:                                                          
-        InsLen.append(int(line.split("\t")[4]) - int(line.split("\t")[3]))        #Insertion length
-        InsClass = line.split("\t")[2]                                            #Classification
-        ID = line.split(";")[2].split("=")[1]                                     #Family name
-        Scaffold.append(line.split("\t")[0])                                      #Scaffold name
-        Start.append(line.split("\t")[3])                                         #Start coordinates
-        End.append(line.split("\t")[4])                                           #End coordinates
-        Name.append("#".join([ID,InsClass]))                                      #Concatenate Fam and Class
-        InType = line.split(";")[3].split("=")[1]                                 #Insertion type (Fragmented or not)                                                   #Ins length
+    for line in f:                                                         
+        #Insertion length
+        InsLen.append(int(line.split("\t")[4]) - int(line.split("\t")[3]))
+        #Classification
+        InsClass = line.split("\t")[2]
+        #Family name
+        ID = line.split(";")[2].split("=")[1]
+        #Scaffold name
+        Scaffold.append(line.split("\t")[0])
+        #Start coordinates
+        Start.append(line.split("\t")[3])
+        #End coordinates
+        End.append(line.split("\t")[4])
+        #Concatenate Fam and Class
+        Name.append("#".join([ID,InsClass]))
+        #Known insertions counter
         if InsClass != "Unknown" :
-            Known_ins += 1                                                        #Known insertions counter
-
+            Known_ins += 1                                                                                                    
+#Mean ins len
 Mean_InsLen = sum(InsLen)/len(InsLen)
+#N. ins
 Ins = len(InsLen)
+
+print("...Done")
 
 ##############################################################################
 #-----------------Extract longest insertions and run Blastx------------------#
 ##############################################################################
 
+#----------------------------Blastx of insertions----------------------------#
+
+print("\n")
+print("Searching for TE protein similarities in the insertions...")
+
 #Dataframe with coordinates of insertions
 Ins_df = pd.DataFrame({"scaffold" : Scaffold, "start" : Start, "end" : End, 
                           "TE" : Name, "Len" : InsLen})
-Nrow = round(len(Ins_df)*0.10)                                                  #Calculate the 10% of the total insertions                                                   
-Ins_df = Ins_df.sort_values(by=['Len'],ascending=False)                         #Sort by Insertion Length
-Ins_df = Ins_df.head(Nrow)                                                      #keep only the 10% opf longest insertions
-Ins_df= Ins_df.sort_index(ascending=True)                                       #Reorder by index
-Ins_df.to_csv("%s_Insertions.bed" %OUT, sep="\t", index=False, header = False)          #Write bed file
+#Calculate the 10% of the total insertions
+Nrow = round(len(Ins_df)*0.10)                                                       
+#Sort by Insertion Length
+Ins_df = Ins_df.sort_values(by=['Len'],ascending=False)                         
+#keep only the 10% opf longest insertions
+Ins_df = Ins_df.head(Nrow)
+#Reorder by index
+Ins_df= Ins_df.sort_index(ascending=True)
+#Write bed file
+Ins_df.to_csv("%s_Insertions.bed" %OUT, sep="\t", index=False, header = False)
 
-
-with open("%s_Insertions.fa" %OUT, 'w') as extracted_faa :                           #Extraction of inserted copies
+#Extraction of insertion copies
+with open("%s_Insertions.fa" %OUT, 'w') as extracted_faa :                            
     subprocess.run(["bedtools", "getfasta", "-name","-fi", 
                     GENOME , "-bed" , "%s_Insertions.bed" %OUT], 
                    stdout=extracted_faa, stderr=subprocess.DEVNULL)
-    
-db = NcbimakeblastdbCommandline(dbtype="prot",                                  #Create blastdb of TE proteins
+
+#Create blastdb of TE proteins
+db = NcbimakeblastdbCommandline(dbtype="prot",                           
                                    input_file= TE_ProtLib)
 stdout, stderr = db()
 
-blastn_cline = NcbiblastxCommandline(query = "%s_Insertions.fa" %OUT, num_threads = N_threads, #Blastx of insertions against a TE protein db
+#Blastx of insertions again TE proteins
+blastn_cline = NcbiblastxCommandline(query = "%s_Insertions.fa" %OUT, num_threads = N_threads,
                                      db=TE_ProtLib, evalue=1e-5, outfmt=6, 
-                                     out="%s_Blast.out" %OUT,max_target_seqs = 1, max_hsps = 1)
+                                     out="%s_Ins_Blast.out" %OUT,max_target_seqs = 1, max_hsps = 1)
 stdout, stderr = blastn_cline()
 
-nBlastxHit = 0                                                                    #Count number of hits
-with open("%s_Blast.out" %OUT) as f:
+#Count number of hits
+nBlastxHit = 0
+with open("%s_Ins_Blast.out" %OUT) as f:
     for line in f:
         nBlastxHit += 1
+
+print("...Done")
+        
+#----------------------Blastx of consensus sequences-------------------------#
+print("\n")
+print("Searching for TE protein similarities in the consensus...")
+
+blastn_cline = NcbiblastxCommandline(query = LIBRARY, num_threads = N_threads,
+                                     db=TE_ProtLib, evalue=1e-5, outfmt=6, 
+                                     out="%s_Cons_Blast.out" %OUT,max_target_seqs = 1, max_hsps = 1)
+stdout, stderr = blastn_cline()
+
+#Count number of hits
+nBlastxHit_cons = 0
+with open("%s_Cons_Blast.out" %OUT) as f:
+    for line in f:
+        nBlastxHit_cons += 1
+
+print("...Done")
         
 ##############################################################################
 #--------------------Append results and create ouput df----------------------#
 ##############################################################################
 
+print("\n")
+print("Collecting all results...")
+
 mydict['Name'].append(LibName)
 mydict['Cons.Len'].append(Mean_Len)
-mydict['N.Blastx'].append(nBlastxHit)
+mydict['N.ins_Blastx'].append(nBlastxHit)
+mydict['N.cons_Blastx'].append(nBlastxHit_cons)
 mydict['Known.Ins'].append(Known_ins)
 mydict['N.ins'].append(Ins)
-mydict['N.Short.ins'].append(Nshort)
 mydict['Mean.Ins.Len'].append(Mean_InsLen)
 
 df = pd.DataFrame(mydict)
 df.to_csv("%s_Summary.tsv" %OUT, sep="\t", index=False, header = True)
+
+print("...Done")
